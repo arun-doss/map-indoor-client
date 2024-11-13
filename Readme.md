e4ca19abe93a19c0b0434b03b33d3a03e0ba2375
Arun Doss <arunrock13@gmail.com>
2024-11-13 05:45:18 +0300

    first commit

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Map Your Indoor
+
+Provides indoor maps creation abilities to Engineers and Developers who can generate their own "Apple Indoor IMDF" or "Microsoft Places IMDF" format of maps for their Indoor spaces.
+#### 2D Map editor to draw indoor maps
+Simple client end application using Open layers with Draw Tools for creating Indoor Maps
+
+Steps to Create Indoor Map:
+1. **Your Input** - Pre Existing CAD / Draw your Own
+2. **Updatable GIS Data*
+3. **IMDF format** - Converted  for your Apps
+
---
+++ main.js
@@ -0,0 +1,71 @@
+import Draw from 'ol/interaction/Draw.js';
+import Map from 'ol/Map.js';
+import View from 'ol/View.js';
+import {OSM, Vector as VectorSource} from 'ol/source.js';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer.js';
+import Geolocation from 'ol/Geolocation.js';
+
+
+
+
+
+const raster = new TileLayer({
+  source: new OSM(),
+});
+
+const source = new VectorSource({wrapX: false});
+
+const vector = new VectorLayer({
+  source: source,
+});
+
+const map = new Map({
+  layers: [raster, vector],
+  target: 'map',
+  view: new View({
+    center: [-11000000, 4600000],
+    zoom: 4,
+  }),
+});
+
+const geolocation = new Geolocation({
+    // take the projection to use from the map's view
+    // projection: map.view.getProjection(),
+    tracking: true
+  });
+  // listen to changes in position
+  geolocation.on('change', function(evt) {
+    console.log(geolocation.getPosition());
+  });
+  // listen to error
+  geolocation.on('error', function(evt) {
+    window.console.log(evt.message);
+  });
+
+const typeSelect = document.getElementById('type');
+
+let draw; // global so we can remove it later
+function addInteraction() {
+  const value = typeSelect.value;
+  if (value !== 'None') {
+    draw = new Draw({
+      source: source,
+      type: typeSelect.value,
+    });
+    map.addInteraction(draw);
+  }
+}
+
+/**
+ * Handle change event.
+ */
+typeSelect.onchange = function () {
+  map.removeInteraction(draw);
+  addInteraction();
+};
+
+document.getElementById('undo').addEventListener('click', function () {
+  draw.removeLastPoint();
+});
+
+addInteraction();
